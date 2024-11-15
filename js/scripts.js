b956226ebca25cb595a9104fb0787846bb3c9e60
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-15 20:25:51 +0100

    Header and first card

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 const rootStyles = document.documentElement.style;
+const hamburgerElement = document.getElementById('hamburger');
+const closeElement = document.getElementById('close');
+const menuElement = document.getElementById('menu');
+
+const showMenu = () => {
+  closeElement.classList.toggle('close');
+  hamburgerElement.classList.toggle('close');
+  menuElement.classList.toggle('menu-show');
+};
+hamburgerElement.addEventListener('click', showMenu);
+closeElement.addEventListener('click', showMenu);
