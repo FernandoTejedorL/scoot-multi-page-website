7ed7bcb5cbc1831acf634b6d69e62c2eb7a3b5b9
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-17 22:28:24 +0100

    tablet in progress

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ const rootStyles = document.documentElement.style;
 const hamburgerElement = document.getElementById('hamburger');
 const closeElement = document.getElementById('close');
 const menuElement = document.getElementById('menu');
+const allArrowElements = document.querySelectorAll('.accordion-arrow');
 
 const showMenu = () => {
   closeElement.classList.toggle('close');
@@ -10,3 +11,16 @@ const showMenu = () => {
 };
 hamburgerElement.addEventListener('click', showMenu);
 closeElement.addEventListener('click', showMenu);
+
+const showAccordion = (event) => {
+  event.target.classList.toggle('arrow-turn');
+  console.dir(
+    event.target.parentElement.parentElement.children[1].classList.toggle(
+      'accordion-hide'
+    )
+  );
+};
+
+allArrowElements.forEach((arrow) => {
+  arrow.addEventListener('click', showAccordion);
+});
